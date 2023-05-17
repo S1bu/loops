7a5ea72a73d4e7a9341a0ebb986967a3c879e731
S1bu <74048211+S1bu@users.noreply.github.com>
2023-05-17 16:30:43 +0200

    Add files via upload

---
+++ script/code.js
@@ -0,0 +1,59 @@
+/*let age = 17;
+let salary = 5000;
+//18 and above
+if( age > 17){
+    if (salary >= 5000) {//nested if statement
+        console.log("well done!")
+    }else{//we nedd a else statement whenusing nested if statements
+
+    }
+}else{
+    console.log("try again next year")
+}
+
+*/
+
+// let apheleleCat = "black";
+
+// if( apheleleCat == "black"){
+//     console.log("The cat is black")
+// }else{
+//     console.log("the cat is not black")
+// }
+
+// let jacket = "NavyBlue";
+
+// if( jacket == "NavyBlue"){
+//     console.log("Joel's jacket is Navy Blue");
+// }else{
+//     console.log(`Joel's must buy another jacket`)
+// }
+// let x = 3;
+// let y = 2;
+
+// let check = (x > y)? x:y;
+// console.log(check)
+
+// let numb1 = Number(4);
+// console.log(typeof 4)
+ 
+// let Number = 7;
+// if( typeof numb1 === 'Number'){
+//     console.log('It is  a number')
+// }else{
+// console.log('It is not a number')
+// }
+
+// let data =['Hen']
+
+for (let i= 0; i <= Infinity; i++) {
+   console.log("Taff")
+    
+}
+
+// let data =['henk',9,true,8,'j',4.7]
+// for(let i = 0; i < data.length ; i++){
+//     if(typeof data[1] == "number"){ //if statement to display numbers only
+//         console.log(data[1]);
+//     }
+// }
